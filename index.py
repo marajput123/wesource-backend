385c24fb99bde98b3f78e67dc27f79831a08bb96
kaynoedah <kaynoedah@gmail.com>
2021-10-05 22:59:59 -0400

    Black format index.py

--- index.py
+++ index.py
@@ -2,20 +2,21 @@ from flask import Flask, jsonify
 from pymongo import MongoClient
 
 # Database URL
-MONGODB_URL = 'mongodb+srv://capstone:Capstone123@wesourcecluster01.ctf3x.mongodb.net/WeSource?ssl=true&ssl_cert_reqs=CERT_NONE&retryWrites=true&w=majority'
+MONGODB_URL = "mongodb+srv://capstone:Capstone123@wesourcecluster01.ctf3x.mongodb.net/WeSource?ssl=true&ssl_cert_reqs=CERT_NONE&retryWrites=true&w=majority"
 
 # Database Connection
 client = MongoClient(MONGODB_URL, connect=False)
 
 # Databases
-testDatabase = client['WeSource']
+testDatabase = client["WeSource"]
 
 # Collections
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello_world():
-    
-    dict = {'hello':'world!'}
+
+    dict = {"hello": "world!"}
     return jsonify(dict)
