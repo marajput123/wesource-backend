2df9e55e1326bfe133b7ad01babed4c806b244c9
kaynoedah <74389780+kaynoedah@users.noreply.github.com>
2021-10-10 20:16:09 -0400

    Add basic workflow for python build (#3)
    * Add initial workflows for linting
    
    * Black format index.py
    
    * Fix pylint in index.py
    
    * Disable pytest since no tests are written
    
    Co-authored-by: kaynoedah <chelseylin@DESKTOP-QICQLVA.localdomain>

--- index.py
+++ index.py
@@ -1,21 +1,27 @@
+""" Main app call """
+
 from flask import Flask, jsonify
 from pymongo import MongoClient
 
 # Database URL
-MONGODB_URL = 'mongodb+srv://capstone:Capstone123@wesourcecluster01.ctf3x.mongodb.net/WeSource?ssl=true&ssl_cert_reqs=CERT_NONE&retryWrites=true&w=majority'
+MONGODB_URL = "mongodb+srv://capstone:Capstone123@wesourcecluster01.ctf3x.mongodb.net/WeSource?ssl=true&ssl_cert_reqs=CERT_NONE&retryWrites=true&w=majority"  # pylint: disable=line-too-long
 
 # Database Connection
 client = MongoClient(MONGODB_URL, connect=False)
 
 # Databases
-testDatabase = client['WeSource']
+testDatabase = client["WeSource"]
 
 # Collections
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello_world():
-    
-    dict = {'hello':'world!'}
-    return jsonify(dict)
+    """
+    Dummy root route
+    """
+
+    resp = {"hello": "world!"}
+    return jsonify(resp)
