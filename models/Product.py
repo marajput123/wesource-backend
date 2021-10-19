2cab1c80772632550c37c136ce9862578f529053
AnimeSam <sphung@uncc.edu>
2021-10-18 23:43:19 -0400

    Changed Product price IntField to DecimalField, corrected typo defualt -> default and added more required in fields

--- models/Product.py
+++ models/Product.py
@@ -2,15 +2,22 @@
 from bson.objectid import ObjectId
 from mongoengine.base.fields import ObjectIdField
 from mongoengine.document import Document
-from mongoengine.fields import IntField, ListField, ReferenceField, StringField
+from mongoengine.fields import (
+    IntField,
+    ListField,
+    ReferenceField,
+    StringField,
+    DecimalField,
+)
 
 
 class Product(Document):
     """Product Collection"""
+
     _id = ObjectIdField(default=ObjectId())
     title = StringField(max_length=75, required=True)
     description = StringField(max_length=250, required=True)
-    price = IntField(min_value=0)
-    quantity = IntField(defualt=1)
+    price = DecimalField(min_value=0, precision=2, required=True)
+    quantity = IntField(default=1, required=True)
     imageURL = StringField()
     itemIds = ListField(ReferenceField("Item"), default=[])
