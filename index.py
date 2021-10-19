31d10deaab8ee253ecfee4e589b8e5bd3dfb3f8f
AnimeSam <sphung@uncc.edu>
2021-10-18 23:39:24 -0400

    Set up resource endpoints, used custom db for testing, added various imports like model imports

--- index.py
+++ index.py
@@ -3,15 +3,32 @@ from flask import Flask, jsonify
 from mongoengine import connect
 from util.decorators.errorHandler import exception_handler
 
+# Needed to run flask app with flask_restful
+import flask.scaffold
+
+flask.helpers._endpoint_from_view_func = flask.scaffold._endpoint_from_view_func
+# from product import product_api
+from flask_restful import Resource, Api
+from product import Product, Products
+
+# Have to import models
+import models.User
+import models.Item
+import models.Product
+
 
 # Database URL
-MONGODB_URL = "mongodb+srv://capstone:Capstone123@wesourcecluster01.ctf3x.mongodb.net/WesourceDatabase?retryWrites=true&w=majority"  # pylint: disable=line-too-long
+MONGODB_URL = "mongodb+srv://test2:123@cluster0.fujai.mongodb.net/test"  # pylint: disable=line-too-long
 
 # Database Connection
 connect(host=MONGODB_URL)
 
 
 app = Flask(__name__)
+api = Api(app)
+api.add_resource(Product, "/api/product/<string:product_id>", endpoint="product_by_id")
+api.add_resource(Product, "/api/product", endpoint="product")
+api.add_resource(Products, "/api/products")
 
 
 @app.route("/", methods=["POST", "GET"])
