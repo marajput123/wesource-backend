f951c490039216c832b467bb9cc325a765982bdb
AnimeSam <sphung@uncc.edu>
2021-10-18 23:40:44 -0400

    Redid crud rest api using flask-restful and mongoengine

--- product.py
+++ product.py
@@ -1,63 +1,58 @@
-from flask import request, jsonify, Blueprint, abort
-from pymongo import ReturnDocument
-from db import productCollection
-from bson import json_util
-from bson.objectid import ObjectId
-# import json
+from models.Product import Product as productModel
+from flask_restful import Resource, reqparse, abort
 
-product_api = Blueprint('product', __name__)
+product_args = reqparse.RequestParser()
+product_args.add_argument("_id", type=str, help="Product ID")
+product_args.add_argument("title", type=str, help="Product Name")
+product_args.add_argument("description", type=str, help="Product Description")
+product_args.add_argument("price", type=float, help="Product Price")
+product_args.add_argument("quantity", type=int, help="Product Quantity")
+product_args.add_argument("imageURL", type=str, help="Product Image URL")
+product_args.add_argument(
+    "itemIds", type=str, help="A List of the Item Ids", action="append"
+)
 
 
-@product_api.route('/api/product/<product_id>', methods=['GET', 'PUT', 'DELETE'])
-def product(product_id):
-    # GET request
-    if request.method == 'GET':
-        product = productCollection.find_one({'_id': ObjectId(product_id)})
-        if product is None:
+class Product(Resource):
+    def get(self, product_id):
+        product = productModel.objects(_id=product_id)
+        if len(product) == 0:
             abort(404)
-        # Turn ObjectId into a string when returning json data
-        product['_id'] = str(product['_id'])
-        # json_util.dumps is used to serialize mongodb's ObjectId
-        # then the use of json.loads to deserialize into python dictionary
-        # jsonify would just serialize the data to a JSON
-        return jsonify(product), 200
-    # PUT request which updates the product
-    elif request.method == 'PUT':
-        updatedProduct = request.form.to_dict()
-        # filter/find by _id field must be an ObjectId else the product_id string will replace ObjectId
-        product = productCollection.find_one_and_update({'_id': ObjectId(product_id)},
-                                                        {'$set': {'age': updatedProduct['age'],
-                                                                  'name': updatedProduct['name'],
-                                                                  'race': updatedProduct['race']}},
-                                                        upsert=True,
-                                                        return_document=ReturnDocument.AFTER)
-        if product is None:
+        return product.to_json(), 200
+
+    def post(self):
+        newProduct = productModel(**product_args.parse_args())
+        newProduct.save()
+        return 201
+
+    # NOTE this assumes that the userProduct does not contain None values
+    # As such all fields should be required
+    def put(self, product_id):
+        userProduct = productModel(**product_args.parse_args())
+        try:
+            productModel.objects(_id=product_id).update_one(
+                title=userProduct.title,
+                description=userProduct.description,
+                price=userProduct.price,
+                quantity=userProduct.quantity,
+                imageURL=userProduct.imageURL,
+                itemIds=userProduct.itemIds,
+            )
+        except Exception as e:
+            print(e)
             abort(404)
-        return jsonify(updatedProduct), 200
-    # DELETE request
-    else:
-        removedProduct = productCollection.find_one_and_delete(
-            {'_id': ObjectId(product_id)})
-        if removedProduct is None:
+        return 200
+
+    def delete(self, product_id):
+        product = productModel.objects(_id=product_id)
+        if len(product) == 0:
             abort(404)
-        removedProduct['_id'] = str(removedProduct['_id'])
-        return jsonify(removedProduct), 202
+        else:
+            product.delete()
+        return 202
 
 
-@product_api.route('/api/product', methods=['POST'])
-def addProduct():
-    # If flat is True the returned dict will only have the first item present, if flat is False all values will be returned as lists.
-    product = productCollection.insert_one(request.form.to_dict(flat=True))
-    # Access pymongo.results.InsertOneResult object using the inserted_id
-    addedProduct = productCollection.find_one({'_id': product.inserted_id})
-    addedProduct['_id'] = str(addedProduct['_id'])
-    print(addedProduct)
-    return jsonify(addedProduct), 202
-
-
-@product_api.route('/api/products', methods=['GET'])
-def getProducts():
-    products = productCollection.find()
-    if products is None:
-        abort(404)
-    return jsonify(json_util.dumps(products)), 200
+class Products(Resource):
+    def get(self):
+        products = productModel.objects.all()
+        return products.to_json(), 200
