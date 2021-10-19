f150b269c5db15141a75ec706df96ffb170fd1e6
AnimeSam <sphung@uncc.edu>
2021-10-18 23:43:47 -0400

    corrected typo defualt -> default and added more required in fields

--- models/Item.py
+++ models/Item.py
@@ -11,6 +11,6 @@ class Item(Document):
     title = StringField(max_length=75, required=True)
     description = StringField(max_length=250, required=True)
     price = IntField(min_value=0, required=True)
-    quantity = IntField(defualt=1)
+    quantity = IntField(default=1)
     imageURL = StringField()
     productId = ReferenceField("Product")
