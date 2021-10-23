dddd5064864031c3531897e85e51badbe7b2927d
kaynoedah <kaynoedah@gmail.com>
2021-10-23 02:18:56 -0400

    Store hashed password in DB instead of plain text

--- models/User.py
+++ models/User.py
@@ -3,13 +3,27 @@ from bson.objectid import ObjectId
 from mongoengine.base.fields import ObjectIdField
 from mongoengine.document import Document
 from mongoengine.fields import EmailField, IntField, StringField
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(Document):
     """User Collection"""
+
     _id = ObjectIdField(default=ObjectId())
     firstName = StringField(max_length=50, required=True)
     lastName = StringField(max_length=50, required=True)
     email = EmailField(max_length=50, required=True, unique=True)
     username = StringField(max_length=30, min_length=6, required=True, unique=True)
-    password = StringField(max_length=24, min_length=6, required=True)
+    password = StringField(required=True)  # password hash
     rating = IntField(default=0)
+
+
+    def hash_password(self, password):
+        """Hash given password and stores result in `password` field"""
+        self.password = generate_password_hash(password)
+
+    def verify_password(self, password):
+        """Check provided password against hashed password stored in database.
+
+        Returns True if password matches, False otherwise.
+        """
+        return check_password_hash(self.password, password)
