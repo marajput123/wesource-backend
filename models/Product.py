fce461bf03bd88ac255c5f22f3e8929565163230
AnimeSam <sphung@uncc.edu>
2021-10-23 17:21:25 -0400

    Changed product item as an embedded document rather than using it as a reference

--- models/Product.py
+++ models/Product.py
@@ -4,20 +4,31 @@ from mongoengine.base.fields import ObjectIdField
 from mongoengine.document import Document
 from mongoengine.fields import (
     IntField,
-    ListField,
-    ReferenceField,
+    EmbeddedDocumentListField,
+    EmbeddedDocument,
     StringField,
     DecimalField,
 )
 
 
+class Item(EmbeddedDocument):
+    """Item Schema"""
+
+    _id = ObjectIdField(default=ObjectId())
+    title = StringField(max_length=75, required=True)
+    description = StringField(max_length=250, required=True)
+    price = DecimalField(min_value=0, required=True)
+    quantity = IntField(default=1)
+    imageURL = StringField()
+
+
 class Product(Document):
-    """Product Collection"""
+    """Product Schema"""
 
     _id = ObjectIdField(default=ObjectId())
     title = StringField(max_length=75, required=True)
     description = StringField(max_length=250, required=True)
     price = DecimalField(min_value=0, precision=2, required=True)
-    quantity = IntField(default=1, required=True)
+    quantity = IntField(default=1)
     imageURL = StringField()
-    itemIds = ListField(ReferenceField("Item"), default=[])
+    items = EmbeddedDocumentListField("Item")
