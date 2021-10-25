3fd88c8d81cccb7b2adacb1f09fc17a4256c1986
Sam Phung <sphung@uncc.edu>
2021-10-24 20:01:41 -0400

    Implemented product class (#13)
    * Initial CRUD endpoints using pymongo
    
    * Added flask-restful==0.3.8
    
    * Set up resource endpoints, used custom db for testing, added various imports like model imports
    
    * Redid crud rest api using flask-restful and mongoengine
    
    * Changed Product price IntField to DecimalField, corrected typo defualt -> default and added more required in fields
    
    * corrected typo defualt -> default and added more required in fields
    
    * Changed to my test db
    
    * Changed flask-restful==0.3.9
    
    * Cleaned up imports
    
    * Tried conforming to pylint specifications
    
    * Disable pylint false-positive saying product_model has no member attribute of objects
    
    * Added pylint: disable=unused-import
    
    * Added pylint: disable=no-self-use for all the functions
    
    * Changed import order
    
    * Embedded product item's into the items field
    
    * Changed product item as an embedded document rather than using it as a reference
    
    * Removed item's related commands
    
    * Commented out product item's route
    
    * Reworked to compensate for the fact that product items are no longer referenced but instead are embedded documents. Turned isntance functions to static methods. Commented out crud for product items.
    
    * Removed product items' related information
    
    * Removed item's related commands
    
    * No longer needed as item is now an embedded document rather than being referenced by product
    
    * Update db_populateScript.py
    
    Reverted back to original db url
    
    * Update index.py
    
    Reverted back to the original db url
    
    * Update product.py
    
    Removed comments regarding product item

--- db_populateScript.py
+++ db_populateScript.py
@@ -2,9 +2,7 @@
 import sys
 from json import load
 from mongoengine import connect
-from models.Item import Item
 from models.Product import Product
-
 from models.User import User
 
 ## Replace it with you DB if you need to
@@ -20,23 +18,16 @@ for arg in sys.argv:
         commands["create_users"] = True
     elif arg == "cp":
         commands["create_products"] = True
-    elif arg == "ci":
-        commands["create_items"] = True
     elif arg == "du":
         commands["delete_users"] = True
     elif arg == "dp":
         commands["delete_products"] = True
-    elif arg == "di":
-        commands["delete_items"] = True
     elif arg == "delete_all":
         commands["delete_all"] = True
 
 USERS = None
 with open(file="./data/users.json", encoding="utf-8") as reader:
     USERS = load(reader)
-ITEMS = None
-with open("./data/items.json", encoding="utf-8") as reader:
-    ITEMS = load(reader)
 PRODUCTS = None
 with open("./data/products.json", encoding="utf-8") as reader:
     PRODUCTS = load(reader)
@@ -51,16 +42,10 @@ for key in commands:
     if key == "create_products":
         for product in PRODUCTS:
             Product(**product).save()
-    if key == "create_items":
-        for item in ITEMS:
-            Item(**item).save()
     if key == "delete_users":
         User.drop_collection()
     if key == "delete_products":
         Product.drop_collection()
-    if key == "delete_items":
-        Item.drop_collection()
     if key == "delete_all":
         User.drop_collection()
-        Item.drop_collection()
         Product.drop_collection()
--- index.py
+++ index.py
@@ -2,12 +2,17 @@
 from http import HTTPStatus
 from flask import Flask, jsonify
 from flask.globals import request
+from flask_restful import Api
 from mongoengine import connect
 
 from models.User import User
 from util.decorators.auth import authenticated, generate_auth_token
 from util.decorators.errorHandler import exception_handler
+from product import Product, Products
 
+# Have to import models to register in the document registry
+import models.User  # pylint: disable=unused-import
+import models.Product  # pylint: disable=unused-import
 
 # Database URL
 MONGODB_URL = "mongodb+srv://capstone:Capstone123@wesourcecluster01.ctf3x.mongodb.net/WesourceDatabase?retryWrites=true&w=majority"  # pylint: disable=line-too-long
@@ -17,6 +22,12 @@ connect(host=MONGODB_URL)
 
 
 app = Flask(__name__)
+api = Api(app)
+
+# Product routes
+api.add_resource(Product, "/api/product/<string:product_id>", endpoint="product_by_id")
+api.add_resource(Product, "/api/product", endpoint="product")
+api.add_resource(Products, "/api/products")
 
 
 @app.route("/", methods=["POST", "GET"])
